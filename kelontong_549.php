9e32586f298f58ba5cf28d6f526669a56fae9087
AdeliaCristyanaDewanti <adeliadea873@gmail.com>
2024-06-10 10:46:34 +0700

    Add files via upload

---
+++ kelontong_549.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class kelontong_549 extends Model
+{
+    protected $table = 'kelontong_549';
+    protected $fillable = ['produk', 'kategori', 'pelanggan', 'pesanan'];
+}
